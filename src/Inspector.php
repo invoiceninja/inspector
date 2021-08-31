ea4e26aaad0e67efc2037e82a824367a73b4753f
Benjamin Beganović <k1pstabug@gmail.com>
2021-08-31 11:08:00 +0200

    Specify `connection` for schema manager

--- src/Inspector.php
+++ src/Inspector.php
@@ -2,7 +2,26 @@
 
 namespace InvoiceNinja\Inspector;
 
+use Illuminate\Support\Facades\DB;
+
 class Inspector
 {
-    // Build your next great package.
+    protected string $connectionName = '';
+
+    public function setConnectionName(string $connectionName): self
+    {
+        $this->connectionName = $connectionName;
+
+        return $this;
+    }
+
+    public function getConnectionName(): string
+    {
+        return $this->connectionName;
+    }
+
+    public function getSchemaManager()
+    {
+        return DB::connection($this->connectionName)->getDoctrineSchemaManager();
+    }
 }
