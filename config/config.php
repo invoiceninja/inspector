30e846cf8fae1f99aba27d1c04b9180740dcf625
Benjamin Beganović <k1pstabug@gmail.com>
2021-09-01 13:07:38 +0200

    Basic configuration file

--- config/config.php
+++ config/config.php
@@ -2,4 +2,29 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Show tables
+    |--------------------------------------------------------------------------
+    |
+    | This will let us control the tables we want to show. To show
+    | all the tables, use asterisk symbol.
+    |
+    */
+    'show_tables' => [
+        '*',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Hide tables
+    |--------------------------------------------------------------------------
+    |
+    | Opposite of `show_tables`. This will let you specify which tables,
+    | should be hidden. However, this has priority over `show_tables`.
+    | If you specify asterisk symbol here, all tables will be hidden.
+    |
+    */
+
+    'hide_tables' => [],
 ];
