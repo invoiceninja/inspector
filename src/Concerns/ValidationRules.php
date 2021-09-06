00335e8f8c2b9794fdf70e805933398a4ac18c17
Benjamin Beganović <k1pstabug@gmail.com>
2021-09-06 19:32:56 +0200

    Add `required` validator

--- src/Concerns/ValidationRules.php
+++ src/Concerns/ValidationRules.php
@@ -13,10 +13,28 @@ trait ValidationRules
         $column = $columns[$column];
         $rules = [];
 
+        $this->required($column, $rules);
         $this->type($column, $rules);
         $this->length($column, $rules);
 
         return $rules;
+    }
+
+    public function transformFields(array $requestBody): array
+    {
+        if (\array_key_exists('created_at', $requestBody)) {
+            $requestBody['created_at'] = \Carbon\Carbon::createFromFormat('Y-m-d\TH:i', $requestBody['created_at'])->format('Y-m-d H:i:s');
+        }
+
+        if (\array_key_exists('updated_at', $requestBody)) {
+            $requestBody['updated_at'] = \Carbon\Carbon::createFromFormat('Y-m-d\TH:i', $requestBody['updated_at'])->format('Y-m-d H:i:s');
+        }
+
+        if (\array_key_exists('deleted_at', $requestBody)) {
+            $requestBody['deleted_at'] = \Carbon\Carbon::createFromFormat('Y-m-d\TH:i', $requestBody['deleted_at'])->format('Y-m-d H:i:s');
+        }
+
+        return $requestBody;
     }
 
     private static function type(\Doctrine\DBAL\Schema\Column $column, &$rules): void
@@ -37,6 +55,19 @@ trait ValidationRules
             return;
         }
 
+        if ($column->getName() === 'created_at' || $column->getName() === 'updated_at' || $column->getName() === 'deleted_at') {
+            return;
+        }
+
         \array_push($rules, \sprintf('max:%s', $column->getLength()));
+    }
+
+    private static function required(\Doctrine\DBAL\Schema\Column $column, &$rules): void
+    {
+        if ($column->getNotNull()) {
+            \array_push($rules, 'required');
+        } else {
+            \array_push($rules, 'nullable');
+        }
     }
 }
