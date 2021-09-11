0fd1b469cd398ef1f8123d5ca9431aa9364aacc7
Benjamin Beganović <k1pstabug@gmail.com>
2021-09-11 13:30:01 +0200

    Make `Record` component renderless

--- resources/views/components/record.blade.php
+++ resources/views/components/record.blade.php
@@ -1,27 +1,26 @@
-<form 
-    method="POST"
+<form method="POST"
     action="{{ $updateRouteName ? route($updateRouteName, ['table' => $table->getName(), 'id' => $record->id]) : '#' }}">
 
     @csrf
     @method('put')
 
     @foreach ($columns as $column)
-        <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
-            <dt class="text-sm font-medium text-gray-500">
+        <div class="{{ $attributes['input-wrapper-class'] ?? '' }}">
+            <dt class="{{ $attributes['input-label-class'] ?? '' }}">
                 {{ $column->getName() }}
             </dt>
 
-            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+            <dd class="{{ $attributes['input-field-wrapper-class'] ?? '' }}">
                 <x-inspector-input :column="$column" value="{{ $record->{$column->getName()} }}" />
 
                 @error($column->getName())
-                    <span class="ml-2">{{ $message }}</span>
+                    <span class="{{ $attributes['validation-class'] ?? '' }}">{{ $message }}</span>
                 @enderror
             </dd>
         </div>
     @endforeach
 
-    <div class="flex justify-end">
-        <button class="bg-gray-100 py-2 px-4 rounded">Save</button>
+    <div class="{{ $attributes['form-button-wrapper-class'] ?? '' }}">
+        <button class="{{ $attributes['form-button-class'] ?? '' }}">Save</button>
     </div>
 </form>
