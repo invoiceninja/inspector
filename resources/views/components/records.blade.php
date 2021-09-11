bc8ece569176e63d56ce9a855b407453eaa8a4b7
Benjamin Beganović <k1pstabug@gmail.com>
2021-09-11 13:30:07 +0200

    Make `Records` component renderless

--- resources/views/components/records.blade.php
+++ resources/views/components/records.blade.php
@@ -1,26 +1,22 @@
-<div class="my-6">
-    <span class="uppercase bg-gray-100 rounded py-1 px-2 text-xs">Records</span>
-</div>
-
-<div class="overflow-x-auto">
-    <table class="w-full">
+<div class="{{ $attributes['records-wrapper-class'] ?? '' }}">
+    <table class="{{ $attributes['table-class'] ?? '' }}">
         <thead>
-            <td class="px-4 py-2 text-gray-900 border">#</td>
+            <td class="{{ $attributes['td-class'] ?? '' }}">#</td>
             @foreach ($columns as $column => $properties)
-                <td class="px-4 py-2 text-gray-900 border">{{ $column }}</td>
+                <td class="{{ $attributes['td-class'] ?? '' }}">{{ $column }}</td>
             @endforeach
         </thead>
         <tbody>
             @foreach ($records as $row)
                 <tr>
-                    <td class="px-4 py-2 text-gray-900 border">
+                    <td class="{{ $attributes['td-class'] ?? '' }}">
                         @if ($showRouteName)
                             <a
-                                href="{{ route($showRouteName, ['table' => $table->getName(), 'id' => $row->id]) }}">View</a>
+                                href="{{ route($showRouteName, ['table' => $table->getName(), 'id' => $row->id]) }}">{{ $attributes['link-label'] ?? 'View' }}</a>
                         @endif
                     </td>
                     @foreach ($columns as $column => $properties)
-                        <td class="px-4 py-2 text-gray-900 border">{{ $row->{$column} }}</td>
+                        <td class="{{ $attributes['td-class'] ?? '' }}">{{ $row->{$column} }}</td>
                     @endforeach
                 </tr>
             @endforeach
@@ -28,7 +24,7 @@
     </table>
 
     @if ($records instanceof \Illuminate\Pagination\LengthAwarePaginator)
-        <div class="mt-4">
+        <div class="{{ $attributes['pagination-wrapper-class'] ?? '' }}">
             {{ $records->links() }}
         </div>
     @endif
