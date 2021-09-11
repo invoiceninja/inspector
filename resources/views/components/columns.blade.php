3fe4273e7e4d19762fc688a695c49941c8a5b2fe
Benjamin Beganović <k1pstabug@gmail.com>
2021-09-11 13:29:55 +0200

    Make `Columns` component renderless

--- resources/views/components/columns.blade.php
+++ resources/views/components/columns.blade.php
@@ -1,18 +1,14 @@
-<div class="my-6">
-    <span class="uppercase bg-gray-100 rounded py-1 px-2 text-xs">Columns</span>
-
-    <table class="w-full mt-4">
-        <thead>
-            <td class="px-4 py-2 text-gray-900 border">Column</td>
-            <td class="px-4 py-2 text-gray-900 border">Type</td>
-        </thead>
-        <tbody>
-            @foreach ($columns as $column => $properties)
-                <tr>
-                    <td class="px-4 py-2 text-gray-900 border">{{ $column }}</td>
-                    <td class="px-4 py-2 text-gray-900 border">{{ $properties->getType()->getName() }} </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+<table class="{{ $attributes['table-class'] ?? '' }}">
+    <thead>
+        <td class="{{ $attributes['td-class'] ?? '' }}">Column</td>
+        <td class="{{ $attributes['td-class'] ?? '' }}">Type</td>
+    </thead>
+    <tbody>
+        @foreach ($columns as $column => $properties)
+            <tr>
+                <td class="{{ $attributes['td-class'] ?? '' }}">{{ $column }}</td>
+                <td class="{{ $attributes['td-class'] ?? '' }}">{{ $properties->getType()->getName() }} </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
